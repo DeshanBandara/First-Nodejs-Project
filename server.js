dc747c891a64cd2b3984af1080a78f81ab3b6234
Deshan Bandara <126126025+DeshanBandara@users.noreply.github.com>
2023-08-31 17:58:22 +0530

    modified

---
+++ server.js
@@ -0,0 +1,75 @@
+var express = require("express");
+var app = express();
+var db = require("./database.js");
+const sqlite3 = require("sqlite3");
+var bodyParser = require("body-parser");
+const { request, response } = require("express");
+app.use(bodyParser.json());
+
+let HTTP_PORT = 8050;
+
+app.listen(HTTP_PORT, () => {
+    console.log("Server is running on %PORT%".replace("%PORT%", HTTP_PORT))
+});
+
+app.post("/api/customer", (req, res, next) => {
+    try {
+        if (!req.body) {
+            res.status(400).json({ "error": "Missing request body" });
+            return;
+        }
+
+        const {
+            name,
+            address,
+            email,
+            dateOfBirth,
+            gender,
+            age,
+            cardHolderName,
+            cardNumber,
+            expiryDate,
+            cvv,
+            timeStamp
+        } = req.body;
+
+        var sql = 'INSERT INTO customer (name,address,email,dateOfBirth,gender,age,cardHolderName,cardNumber,expiryDate,cvv,timeStamp) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
+        var params = [name, address, email, dateOfBirth, gender, age, cardHolderName, cardNumber, expiryDate, cvv, timeStamp];
+
+        db.run(sql, params, function (err, result) {
+            if (err) {
+                res.status(400).json({ "error": err.message });
+                return;
+            } else {
+                res.json({
+                    "message": "customer A.D. Lakith Sharmasiri has registered",
+                    "data": req.body,
+                    "customerId": this.lastID
+                });
+            }
+        });
+    } catch (E) {
+        res.status(400).send(E);
+    }
+});
+
+app.get("/api/customer", (req, res, next) => {
+    try {
+        var sql = "select * from customer"
+        var params = []
+        db.all(sql, params, (err, rows) => {
+            if (err) {
+                res.status(400).json({ "error": err.message });
+                return;
+            }
+            res.json({
+                "message": "customer A.D. Lakith Sharmasiri has registered",
+                "data": rows
+            })
+        });
+    } catch (E) {
+        res.status(400).send(E);
+    }
+});
+
+
